7aad3047a0268db05044746267c5e60ea3a1cc5f
Thang Luong <luongththang@gmail.com>
2019-11-22 22:06:39 +0700

    Init history screen

--- App.js
+++ App.js
@@ -1,42 +1,41 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React, { Component } from 'react';
 
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import Tabs from './src/components/Tabs';
 
 const RootStack = createStackNavigator(
-  {
-    Tabs: {
-      screen: Tabs,
-      navigationOptions: {
-        header: null,
-        gesturesEnabled: false,
+   {
+      Tabs: {
+         screen: Tabs,
+         navigationOptions: {
+            header: null,
+            gesturesEnabled: false,
+         },
+         //  },
+         //  Dashboard: {
+         //     screen: Dashboard,
+         //     navigationOptions: {
+         //        header: null,
+         //        gesturesEnabled: false
+         //     }
+         //  },
       },
-      //  },
-      //  Dashboard: {
-      //     screen: Dashboard,
-      //     navigationOptions: {
-      //        header: null,
-      //        gesturesEnabled: false
-      //     }
-      //  },
-    },
-  },
-  {
-    initialRouteName: 'Tabs',
-  },
+   },
+   {
+      initialRouteName: 'Tabs',
+   },
 );
 const AppContainer = createAppContainer(RootStack);
 export default class App extends Component {
-  render() {
-    return (
-      <AppContainer
-        ref={nav => {
-          this.navigator = nav;
-        }}
-      />
-    );
-  }
+   render() {
+      return (
+         <AppContainer
+            ref={nav => {
+               this.navigator = nav;
+            }}
+         />
+      );
+   }
 }
