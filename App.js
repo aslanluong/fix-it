507e0edcf4141adca7d4fb7818c7b91852742747
namnnb47 <namnnb47@gmail.com>
2019-11-28 14:25:56 +0700

    update rating

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { createAppContainer } from 'react-navigation';
 
-<<<<<<< HEAD
+
 
 import Rate from './src/Rate';
 
@@ -15,44 +15,3 @@ export default class App extends Component {
     )
   }
 }
-=======
-import Tabs from './src/components/Tabs';
-//import Main from './src/components/TimeLines/TineLine'
-import Main from './src/components/Rating/Rating'
-
-// const RootStack = createStackNavigator(
-//    {
-//       Tabs: {
-//          screen: Tabs,
-//          navigationOptions: {
-//             header: null,
-//             gesturesEnabled: false,
-//          },
-//          //  },
-//          //  Dashboard: {
-//          //     screen: Dashboard,
-//          //     navigationOptions: {
-//          //        header: null,
-//          //        gesturesEnabled: false
-//          //     }
-//          //  },
-//       },
-//    },
-//    {
-//       initialRouteName: 'Tabs',
-//    },
-// );
-// const AppContainer = createAppContainer(RootStack);
-// export default class App extends Component {
-//    render() {
-//       return (
-//          <AppContainer
-//             ref={nav => {
-//                this.navigator = nav;
-//             }}
-//          />
-//       );
-//    }
-// }
-export default Main
->>>>>>> 69a3b106a36e98ffa7c86b3255b2b1328e3147d9
