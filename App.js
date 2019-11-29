c36ab2abac32726ba34b2370f2d8e14f92a89ecb
Thang Luong <luongththang@gmail.com>
2019-11-30 00:30:08 +0700

    commit

--- App.js
+++ App.js
@@ -1,18 +1,69 @@
 import React, { Component } from 'react';
 
 import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
+import Tabs from './src/components/Tabs';
+import RequestDetails from './src/components/Tabs/RequestDetails';
+import NavigationService from './src/services/navigate';
+import ProfileDetails from './src/components/Tabs/ProfileDetails';
+import ProfileEditor from './src/components/Tabs/ProfileEditor';
 
-
-import Rate from './src/Rate';
-import Timeline from './src/Timeline';
-
-
-
+const RootStack = createStackNavigator(
+   {
+      Tabs: {
+         screen: Tabs,
+         navigationOptions: {
+            header: null,
+            gesturesEnabled: false
+         }
+      },
+      RequestDetails: {
+         screen: RequestDetails,
+         navigationOptions: {
+            header: null,
+            gesturesEnabled: false
+         }
+      },
+      ProfileDetails: {
+         screen: ProfileDetails,
+         navigationOptions: {
+            header: null,
+            gesturesEnabled: false
+         }
+      },
+      ProfileEditor: {
+         screen: ProfileEditor,
+         navigationOptions: {
+            header: null,
+            gesturesEnabled: false
+         }
+         //  },
+         //  Dashboard: {
+         //     screen: Dashboard,
+         //     navigationOptions: {
+         //        header: null,
+         //        gesturesEnabled: false
+         //     }
+         //  },
+      }
+   },
+   {
+      initialRouteName: 'Tabs'
+   }
+);
+const AppContainer = createAppContainer(RootStack);
 export default class App extends Component {
-  render() {
-    return (
-      <Timeline/>
-    )
-  }
+   render() {
+      return (
+         <AppContainer
+            // ref={nav => {
+            //    this.navigator = nav;
+            // }}
+            ref={navigatorRef => {
+               NavigationService.setTopLevelNavigator(navigatorRef);
+            }}
+         />
+      );
+   }
 }
